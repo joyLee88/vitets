543a6b2b3d23acba9d24b2a4a9a357cef92a4d91
joyLee <1021135499@qq.com>
2022-10-10 13:36:47 +0800

    fix: postcss警告

--- create.md
+++ create.md
@@ -190,6 +190,25 @@ export default defineConfig({
 });
 ```
 
+但是发现“build.terserOptions is specified but build.minify is not set to use Terser. Note Vite now defaults to use esbuild for minification. ”
+这个配置似乎不能在 vite 用了
+
+```js
+build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+// 或者不用build，用vite的esbuild
+esbuild: {
+  drop: command === "build" ? ["console", "debugger"] : []
+}
+```
+
 # 2022-10-09
 
 1. 配置项目所有工具库
@@ -933,6 +952,139 @@ css: {
       plugins: [basePxToVw]
     }
   },
+```
+
+出现警告：
+postcss-px-to-viewport: postcss.plugin was deprecated. Migration guide:
+https://evilmartians.com/chronicles/postcss-8-plugin-migration
+安装的 postcss-px-to-viewport 不适配最新版本的 postcss8
+
+yarn add postcss-px-to-viewport-8-plugin --save-dev
+具体配置：看 vite.config
+
+```js
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+//@ts-ignore
+import viteCompression from 'vite-plugin-compression';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+// import pxtovw from "postcss-px-to-viewport";
+// const basePxToVw = pxtovw({
+//   viewportWidth: 375,
+//   viewportUnit: "vw",
+// });
+import postcsspxtoviewport8plugin from 'postcss-px-to-viewport-8-plugin';
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const ENV = loadEnv(mode, __dirname);
+  const ISDEV = ENV.VITE_APP_ENV !== 'production';
+  return {
+    base: './', //打包路径
+    plugins: [
+      vue(),
+      // gzip压缩 生产环境生成 .gz 文件
+      viteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: 'gzip',
+        ext: '.gz',
+      }),
+      Components({
+        resolvers: [VantResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/style/main.scss";',
+        },
+      },
+      // 处理打包出现警告 "@charset" must be the first
+      postcss: {
+        plugins: [
+          /* require('postcss-px-to-viewport-8-plugin')({
+           * 2022年8月17日更新，使用require('postcss-px-to-viewport-8-plugin')引入
+           * vite报错：error when starting dev server:Error:
+           * Dynamic require of "postcss-px-to-viewport-8-plugin" is not supported
+           * 故此改为postcsspxtoviewport8plugin({
+           */
+          postcsspxtoviewport8plugin({
+            unitToConvert: 'px', // 需要转换的单位，默认为"px"
+            viewportWidth: 1920, // 视窗的宽度，对应pc设计稿的宽度，一般是1920
+            // viewportHeight: 1080,// 视窗的高度，对应的是我们设计稿的高度
+            unitPrecision: 6, // 单位转换后保留的精度
+            propList: [
+              // 能转化为vw的属性列表
+              '*',
+            ],
+            viewportUnit: 'vw', // 希望使用的视口单位
+            fontViewportUnit: 'vw', // 字体使用的视口单位
+            selectorBlackList: ['el-switch', 'is-checked'], // 需要忽略的CSS选择器，不会转为视口单位，使用原有的px等单位。cretae
+            minPixelValue: 1, // 设置最小的转换数值，如果为1的话，只有大于1的值会被转换
+            mediaQuery: false, // 媒体查询里的单位是否需要转换单位
+            replace: true, // 是否直接更换属性值，而不添加备用属性
+            exclude: /(\/|\\)(node_modules)(\/|\\)/, // 忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
+          }),
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove();
+                }
+              },
+            },
+          },
+        ],
+      },
+    },
+    //启动服务配置
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      open: true,
+      https: false,
+      proxy: {},
+      cors: true,
+    },
+    // 生产环境打包配置
+    build: {
+      // 公共路径(必须有的，不然打包css和js路径问题不加载)
+      publicPath: './',
+      // 打包构建输出路径
+      outDir: 'dist',
+      // 生成静态资源的存放路径
+      assetsDir: 'static',
+      // 构建后是否生成 source map 文件
+      sourcemap: ISDEV,
+      // chunk 大小警告的限制
+      chunkSizeWarningLimit: 700,
+      // 生产环境移除console
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: !ISDEV,
+          drop_debugger: !ISDEV,
+        },
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vlib: ['vue', 'vue-router', 'vuex'],
+          },
+        },
+      },
+    },
+  };
+});
 ```
 
 5. 移动端 click 响应延迟
--- vite.config.ts
+++ vite.config.ts
@@ -1,64 +1,122 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 //@ts-ignore
 import viteCompression from "vite-plugin-compression";
 import Components from "unplugin-vue-components/vite";
 import { VantResolver } from "unplugin-vue-components/resolvers";
-import pxtovw from "postcss-px-to-viewport";
-const basePxToVw = pxtovw({
-  viewportWidth: 750,
-  viewportUnit: "vw",
-});
-
+// import pxtovw from "postcss-px-to-viewport";
+// const basePxToVw = pxtovw({
+//   viewportWidth: 375,
+//   viewportUnit: "vw",
+// });
+import postcsspxtoviewport8plugin from "postcss-px-to-viewport-8-plugin";
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./", //打包路径
-  plugins: [
-    vue(),
-    // gzip压缩 生产环境生成 .gz 文件
-    viteCompression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: "gzip",
-      ext: ".gz",
-    }),
-    Components({
-      resolvers: [VantResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/assets/style/main.scss";',
+export default defineConfig(({ mode }) => {
+  const ENV = loadEnv(mode, __dirname);
+  const ISDEV = ENV.VITE_APP_ENV !== "production";
+  return {
+    base: "./", //打包路径
+    plugins: [
+      vue(),
+      // gzip压缩 生产环境生成 .gz 文件
+      viteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: "gzip",
+        ext: ".gz",
+      }),
+      Components({
+        resolvers: [VantResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
       },
     },
-    postcss: {
-      plugins: [basePxToVw],
-    },
-  },
-  //启动服务配置
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    open: true,
-    https: false,
-    proxy: {},
-  },
-  // 生产环境打包配置
-  //去除 console debugger
-  build: {
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/style/main.scss";',
+        },
+      },
+      // 处理打包出现警告 "@charset" must be the first
+      postcss: {
+        plugins: [
+          /* require('postcss-px-to-viewport-8-plugin')({
+           * 2022年8月17日更新，使用require('postcss-px-to-viewport-8-plugin')引入
+           * vite报错：error when starting dev server:Error:
+           * Dynamic require of "postcss-px-to-viewport-8-plugin" is not supported
+           * 故此改为postcsspxtoviewport8plugin({
+           */
+          postcsspxtoviewport8plugin({
+            unitToConvert: "px", // 需要转换的单位，默认为"px"
+            viewportWidth: 1920, // 视窗的宽度，对应pc设计稿的宽度，一般是1920
+            // viewportHeight: 1080,// 视窗的高度，对应的是我们设计稿的高度
+            unitPrecision: 6, // 单位转换后保留的精度
+            propList: [
+              // 能转化为vw的属性列表
+              "*",
+            ],
+            viewportUnit: "vw", // 希望使用的视口单位
+            fontViewportUnit: "vw", // 字体使用的视口单位
+            selectorBlackList: ["el-switch", "is-checked"], // 需要忽略的CSS选择器，不会转为视口单位，使用原有的px等单位。cretae
+            minPixelValue: 1, // 设置最小的转换数值，如果为1的话，只有大于1的值会被转换
+            mediaQuery: false, // 媒体查询里的单位是否需要转换单位
+            replace: true, // 是否直接更换属性值，而不添加备用属性
+            exclude: /(\/|\\)(node_modules)(\/|\\)/, // 忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
+          }),
+          {
+            postcssPlugin: "internal:charset-removal",
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === "charset") {
+                  atRule.remove();
+                }
+              },
+            },
+          },
+        ],
       },
     },
-  },
+    //启动服务配置
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+      open: true,
+      https: false,
+      proxy: {},
+      cors: true,
+    },
+    // 生产环境打包配置
+    build: {
+      // 公共路径(必须有的，不然打包css和js路径问题不加载)
+      publicPath: "./",
+      // 打包构建输出路径
+      outDir: "dist",
+      // 生成静态资源的存放路径
+      assetsDir: "static",
+      // 构建后是否生成 source map 文件
+      sourcemap: ISDEV,
+      // chunk 大小警告的限制
+      chunkSizeWarningLimit: 700,
+      // 生产环境移除console
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          drop_console: !ISDEV,
+          drop_debugger: !ISDEV,
+        },
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vlib: ["vue", "vue-router", "vuex"],
+          },
+        },
+      },
+    },
+  };
 });
